d6adf5d8a522e7208be2e25f29d954c3bc221111
tee hongchun <chunhalo_@hotmail.com>
2021-08-06 19:30:59 +0800

    Add project files.

---
+++ AnnouncementLibrary/BookingClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnnouncementLibrary
+{
+    public class BookingClass
+    {
+        public int BookId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Request { get; set; }
+        public int TableNo { get; set; }
+        public string ResName { get; set; }
+        public string Email { get; set; }
+
+        
+        
+        
+    }
+}
